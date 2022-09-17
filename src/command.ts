2e0c91133d375ec8bee7c41d7be7ee33648537f2
Kyle Coffey <me@phytolizer.dev>
2022-09-17 17:33:13 -0500

    add updateHeaderGuard

--- src/command.ts
+++ src/command.ts
@@ -3,6 +3,15 @@ import * as vscode from "vscode";
 import dedent = require("dedent");
 import { findLastIndex } from "./util";
 
+class HeaderGuard {
+    constructor(
+        public first: vscode.Range,
+        public second: vscode.Range,
+        public third: vscode.Range,
+        public wasAnsiStyle: boolean
+    ) {}
+}
+
 export function generateHeaderGuard() {
     const editor = vscode.window.activeTextEditor;
     if (editor === undefined) {
@@ -13,15 +22,47 @@ export function generateHeaderGuard() {
     const guard = determineHeaderGuard(currentPath).toUpperCase() + "_";
 
     const snip = dedent(`
-            #ifndef \${1:${guard}}
-            #define \${1:${guard}}
+        #ifndef \${1:${guard}}
+        #define \${1:${guard}}
 
-            \$0
+        \$0
 
-            #endif // \${1:${guard}}
+        #endif // \${1:${guard}}
     `);
 
     editor.insertSnippet(new vscode.SnippetString(snip));
+}
+
+export function updateHeaderGuard() {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined) {
+        return;
+    }
+
+    const currentPath = editor.document.fileName;
+    const guard = determineHeaderGuard(currentPath).toUpperCase() + "_";
+
+    const existingHeaderGuard = findExistingHeaderGuard(editor.document);
+    if (existingHeaderGuard === undefined) {
+        vscode.window.showErrorMessage("No header guard found");
+        return;
+    }
+
+    editor.edit((editBuilder) => {
+        editBuilder.replace(existingHeaderGuard.first, `#ifndef ${guard}`);
+        editBuilder.replace(existingHeaderGuard.second, `#define ${guard}`);
+        if (existingHeaderGuard.wasAnsiStyle) {
+            editBuilder.replace(
+                existingHeaderGuard.third,
+                `#endif /* ${guard} */`
+            );
+        } else {
+            editBuilder.replace(
+                existingHeaderGuard.third,
+                `#endif // ${guard}`
+            );
+        }
+    });
 }
 
 function determineHeaderGuard(currentPath: string): string {
@@ -35,4 +76,60 @@ function determineHeaderGuard(currentPath: string): string {
             ? parts[parts.length - 1]
             : parts.slice(stop + 1).join("_");
     return name.replace(/\W/, "_");
+}
+
+function findExistingHeaderGuard(
+    document: vscode.TextDocument
+): HeaderGuard | undefined {
+    // find all #ifndef in the document
+    let ifndefs: {
+        lineNumber: number;
+        range: vscode.Range;
+        match: RegExpMatchArray;
+    }[] = [];
+    for (let i = 0; i < document.lineCount; i++) {
+        const line = document.lineAt(i);
+        const match = line.text.match(/^#ifndef\s+(.*)/);
+        if (match !== null) {
+            ifndefs.push({ lineNumber: i, range: line.range, match });
+        }
+    }
+    // check if there is a #define for each #ifndef
+    let guard: HeaderGuard | undefined = undefined;
+    for (const ifndef of ifndefs) {
+        const define = document.lineAt(ifndef.lineNumber + 1);
+        const definePat = new RegExp(`^#define\\s+${ifndef.match[1]}`);
+        if (!define.text.match(definePat)) {
+            continue;
+        }
+
+        // find the matching endif
+        let endif: vscode.Range | undefined = undefined;
+        let wasAnsiStyle = false;
+        for (let i = ifndef.lineNumber + 2; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
+            const pat = new RegExp(
+                `^#endif\\s*(//|/\\*)\\s*${ifndef.match[1]}\\s*(?:\\*/)?$`
+            );
+            const match = line.text.match(pat);
+            if (match !== null) {
+                if (match[1] === "/*") {
+                    wasAnsiStyle = true;
+                }
+                endif = line.range;
+                break;
+            }
+        }
+        if (endif !== undefined) {
+            guard = new HeaderGuard(
+                ifndef.range,
+                define.range,
+                endif,
+                wasAnsiStyle
+            );
+            break;
+        }
+    }
+
+    return guard;
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,20 @@
 import * as vscode from "vscode";
 
-import { generateHeaderGuard } from "./command";
+import { generateHeaderGuard, updateHeaderGuard } from "./command";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand(
+    let disposable = vscode.commands.registerCommand(
         "header-guard-generator.generateHeaderGuard",
         generateHeaderGuard
     );
+    context.subscriptions.push(disposable);
 
+    disposable = vscode.commands.registerCommand(
+        "header-guard-generator.updateHeaderGuard",
+        updateHeaderGuard
+    );
     context.subscriptions.push(disposable);
 }
 
